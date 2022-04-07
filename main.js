820c5b88b5e3a4738f9947d2fbb26b43b5b31669
Just Hodges <hello@justhodges.com>
2022-04-06 17:39:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -44,18 +44,18 @@ $(document).ready(function () {
 
 // Carousel Controls
 
-$(document).ready(function() {
-	$('#slider-left-content-previous').on('click', function(){
-		$('#slider-left-content-previous-control').trigger('click');
-	});
-	$('#slider-left-content-next').on('click', function(){
-		$('#slider-left-content-next-control').trigger('click');
-	});
 
-	$('#slider-right-content-previous').on('click', function(){
-		$('#slider-right-content-previous-control').trigger('click');
-	});
-	$('#slider-right-content-next').on('click', function(){
-		$('#slider-right-content-next-control').trigger('click');
-	});
+$('#slider-left-content-previous').on('click', function(){
+	$('#slider-left-content-previous-control').trigger('click');
 });
+$('#slider-left-content-next').on('click', function(){
+	$('#slider-left-content-next-control').trigger('click');
+});
+
+$('#slider-right-content-previous').on('click', function(){
+	$('#slider-right-content-previous-control').trigger('click');
+});
+$('#slider-right-content-next').on('click', function(){
+	$('#slider-right-content-next-control').trigger('click');
+});
+
