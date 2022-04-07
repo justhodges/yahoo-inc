7c0d74909081fadfed33fce9983acfae63101b3d
Just Hodges <hello@justhodges.com>
2022-04-06 17:41:46 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 // Carousel Controls
 
-$(document).ready(function () {
-	$('#slider-left-content-previous').on('click', function(){
-		$('#slider-left-content-previous-control').trigger('click');
-	});
-	$('#slider-left-content-next').on('click', function(){
-		$('#slider-left-content-next-control').trigger('click');
-	});
 
-	$('#slider-right-content-previous').on('click', function(){
-		$('#slider-right-content-previous-control').trigger('click');
-	});
-	$('#slider-right-content-next').on('click', function(){
-		$('#slider-right-content-next-control').trigger('click');
-	});
+$('#slider-left-content-previous').on('click', function(){
+	$('#slider-left-content-previous-control').trigger('click');
 });
+$('#slider-left-content-next').on('click', function(){
+	$('#slider-left-content-next-control').trigger('click');
+});
+
+$('#slider-right-content-previous').on('click', function(){
+	$('#slider-right-content-previous-control').trigger('click');
+});
+$('#slider-right-content-next').on('click', function(){
+	$('#slider-right-content-next-control').trigger('click');
+});
+
 
 
 // Set Footer Copyright Year
